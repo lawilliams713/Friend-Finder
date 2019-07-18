c0cf3df43aed978f41af89b60b4b1bd0f3fe8fa9
lawilliams713 <lawilliams713@gmail.com>
2019-07-18 17:28:03 -0500

    updates and changes

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friendsArray = [
+  {
+    friendName: "Amanda",
+    scores: [      
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+  },
+  {
+    friendName: "Elin",
+    scores: [
+      5,
+      4,
+      1,
+      3,
+      1
+    ]
+  },
+  {
+    friendName: "Tammy",
+    scores: [
+      4,
+      4,
+      4,
+      1,
+      1
+    ]
+  }
+];
+
+module.exports = friendsArray;
